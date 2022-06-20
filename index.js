b0c0e5d451a1ba2af57c6723cf1e899cfc3a75b6
Chase <chasedemaster.tech@gmail.com>
2022-06-20 13:28:29 -0500

    Random us functional but need loop? for future randoms

--- index.js
+++ index.js
@@ -21,21 +21,46 @@
 
 // edit here for git push double check
 
-let randomQuote = fetch("http://api.quotable.io/random")
+fetch("http://api.quotable.io/random")
 .then((res)=>res.json())
-.then((quotes) => {
+.then((randomQuote) => {
     //console.log(quotes)
-})
 
-let random = document.querySelector("#random-click")
+    let random = document.querySelector("#random-click")
 random.addEventListener("click", (e) => {
+    // build counter
+    //conditional, 
+        // if clicked for the first first time load quote BOOM
+        // if clicked for the second time, click count greater than 1
+            //window.location.reload() ((which is new jsom fetch = new quote))
+            // then we run as usual 
+            // set counter back to 1
     e.preventDefault()
     console.log("clicked")
+    //window.setTimeout(function(){location.reload()},25000)
 
     let quoteSpace = document.createElement("p")
     
-    quoteSpace.innerText = `${randomQuote}`
+    quoteSpace.innerText = `${randomQuote.content}`
 
     random.append(quoteSpace)
 })
 console.log(random)
+})
+
+
+
+
+//if it is the first randome quote 
+    //cool, pull quote
+
+//else, if it is the second or more
+    // build click counter
+    //that is when we pull new quote
+
+//
+
+
+
+
+
