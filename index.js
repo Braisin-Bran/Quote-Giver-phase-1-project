3d215e42112794e38384db64df6aaab9ebb11940
Chase <chasedemaster.tech@gmail.com>
2022-06-20 09:43:49 -0500

    psuedocode start

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+//M.inimum V.iable P.roduct Deliverables
+    // User CLICKs random button
+        //grab html via query selector or create element 
+        //if create append as last step
+        //create event listener
+            //define callback/action of click
+
+// User SUBMITs input field
+    //grab html via query selector or create element 
+    //if create append as last step
+    //create event listener
+        //define callback/action of SUBMIT  
+            //is given extensive list of results
+            //If photo is availible then display
+                //else MEGAMIND "No Photo / Not Mario"
+            //Author Wikipedia 
+
+// User CHANGEs search input field and is given a few possibly related results
+
+// edit here for git push double check
