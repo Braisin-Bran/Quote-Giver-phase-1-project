1c3dcbb9eeeec788d5282036165e85102cecef3f
Chase <chasedemaster.tech@gmail.com>
2022-06-21 14:32:23 -0500

    got started on submit (kennedy)

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
-
-
 //M.inimum V.iable P.roduct Deliverables
-    // User CLICKs random button
-        //grab html via query selector or create element 
-        //if create append as last step
-        //create event listener
-            //define callback/action of click
-
-// User SUBMITs input field
-    //grab html via query selector or create element 
-    //if create append as last step
-    //create event listener
-        //define callback/action of SUBMIT  
-            //is given extensive list of results
-            //If photo is availible then display
-                //else MEGAMIND "No Photo / Not Mario"
-            //Author Wikipedia 
 
 // User CHANGEs search input field and is given a few possibly related results
-
-// edit here for git push double check
 
 // fetch("http://api.quotable.io/random")
 // .then((res)=>res.json())
 // .then((randomQuote) => {
-    //console.log(quotes)
-    
-    let random = document.querySelector("#random-click")
-        random.addEventListener("click", () => {
-        fetch("http://api.quotable.io/random")
-        .then((res)=>res.json())
-        .then((randomQuote) => {
-            let quoteSpace = document.querySelector("#quote")
-            console.log(randomQuote)
-            quoteSpace.innerText = randomQuote.content
-            random.append(quoteSpace)
-        })
-})
+//console.log(quotes)
+
+let random = document.querySelector("#random-click");
+random.addEventListener("click", () => {
+  fetch("http://api.quotable.io/random")
+    .then((res) => res.json())
+    .then((randomQuote) => {
+      let quoteSpace = document.querySelector("#quote");
+      console.log(randomQuote);
+      quoteSpace.innerText = randomQuote.content;
+      random.append(quoteSpace);
+    });
+});
+
+// grab html via query selector
+let form = document.querySelector("#author");
+console.log(form);
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log("submitted");
+  //create event listener
+  fetch(
+    `http://api.quotable.io/search/quotes?query=${searchResult}&fields=author`
+  )
+    .then((res) => res.json())
+    .then((authors) => {
+      console.log(authors);
+      let searchResult = document.querySelector("#search-bar").value;
+      let author = authors.filter(searchResult);
+      // e.preventDefault();
+      //filter by "author.value"
+      let quotes = document.createElement("p");
+      quotes.innerText = `${author.slug}`;
+      searchResult.append(p);
+    });
+});
+
+//is given extensive list of results
+//If photo is availible then display
+//else MEGAMIND "No Photo / Not Mario"
+//Author Wikipedia
